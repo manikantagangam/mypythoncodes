c026c86a64511d30a6f06b47f046c4a348734bf4
Manikanta Gangam <manikantagangam@gmail.com.com>
2021-05-03 22:03:20 +0530

    changed initialisation of q

--- kite_star.py
+++ kite_star.py
@@ -1,6 +1,7 @@
 int_lines = int(input("please input number of lines to print: "))
 
 int_middle_line = (int_lines + 1)/2 if int_lines % 2 == 1 else (int_lines)/2 
+q = 1 if int_lines % 2 == 0 else 0
 
 for i in range(1, int_lines+1):
    if (i<=int_middle_line):
@@ -8,7 +9,6 @@ for i in range(1, int_lines+1):
       y=i*"*"
       z=(i-1)*"*"     
    else:
-      q = 1 if int_lines % 2 == 0 else 0
       x=int(i-int_middle_line - q)*" "
       y=(int_lines+1-i)*"*"
       z=(int_lines-i)*"*"
