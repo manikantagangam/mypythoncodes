32ed3e62adffb2ffe5f79d771340b0f953f654ce
Manikanta Gangam <manikantagangam@gmail.com.com>
2021-11-07 09:39:26 +0530

    circle_star, rectangle_star codes

---
+++ rectangle_star.py
@@ -0,0 +1,8 @@
+length = int(input("please input the length of rectangle"))
+width = int(input("please input the width of rectangle"))
+
+for i in range(1,(width+1)):
+    rectangle = length*"*"
+
+    print(rectangle)
+
